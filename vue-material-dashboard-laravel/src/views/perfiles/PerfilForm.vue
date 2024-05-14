e8416198b7893201f91e9734e2e12d962c3c1422
Guillermo Vallejo <guillermo.vallejo@gmail.com>
2024-05-13 19:11:10 -0500

    Cambio visula en vista PerfilForm.vue

--- vue-material-dashboard-laravel/src/views/perfiles/PerfilForm.vue
+++ vue-material-dashboard-laravel/src/views/perfiles/PerfilForm.vue
@@ -1,15 +1,45 @@
 <template>
-  <div>
-    <h1>{{ id ? 'Editar Perfil' : 'Crear Perfil' }}</h1>
-    <form @submit.prevent="submitForm">
-      <input v-model="perfil.nombre" placeholder="Nombre del Perfil" />
-      <button type="submit">Guardar</button>
-    </form>
+  <div class="py-4 container-fluid">
+    <div class="mt-4 user">
+      <div class="col-12">
+        <div class="card">
+          <!-- Card header -->
+          <div class="card-header border-bottom">
+            <div class="user d-flex align-items-center">
+              <div class="col-6">
+                <h5 class="mb-0 ms-0">{{ id ? 'Editar Perfil' : 'Crear Perfil' }}</h5>
+              </div>
+            </div>
+          </div>
+
+          <!-- Card body -->
+          <div class="px-0 pb-0 card-body">
+            <form @submit.prevent="submitForm">
+              <div class="mb-3">
+                <material-input
+                    id="nombre"
+                    label="Nombre del Perfil"
+                    variant="static"
+                    v-model:value="perfil.nombre"
+                    name="nombre" />
+              </div>
+              <div class="text-end">
+                <material-button type="submit" color="dark" variant="gradient">
+                  Guardar
+                </material-button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import MaterialButton from "@/components/MaterialButton.vue";
+import MaterialInput from "@/components/MaterialInput.vue";
 
 export default {
   props: {
@@ -17,6 +47,10 @@ export default {
       type: [String, Number],
       default: null
     }
+  },
+  components: {
+    MaterialButton,
+    MaterialInput
   },
   data() {
     return {
@@ -55,3 +89,26 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.card {
+  margin: 20px 0;
+}
+
+.card-header {
+  padding: 15px;
+  background-color: #f8f9fa;
+}
+
+.card-body {
+  padding: 15px;
+}
+
+.mb-3 {
+  margin-bottom: 1rem;
+}
+
+.text-end {
+  text-align: right;
+}
+</style>
